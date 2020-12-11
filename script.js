8fd773875d6caaa419ac3a11cff088843fe8b88b
Abhijay <fornitefrogs@gmail.com>
2020-12-11 14:22:53 -0800

    added css for buttons

---
+++ script.js
@@ -0,0 +1,16 @@
+const sounds = ["bruh"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
