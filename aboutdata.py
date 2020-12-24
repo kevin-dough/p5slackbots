a8cbdfb6cf9e68350c928d07f7ecea93796fd43c
Kevin Do <kevin12345do@gmail.com>
2020-12-24 12:44:44 -0800

    animation for socials

--- aboutdata.py
+++ aboutdata.py
@@ -1,31 +1,34 @@
 def kevin():
     name = "kevin do"
+    color = "#ff458a"
     firstname = "kevin"
     nickname = "lord visuality"
-    github = "https://github.com/kevin-dough"
-    insta = "https://www.instagram.com/_kevintdo_/"
+    social1 = "https://github.com/kevin-dough"
+    social2 = "https://www.instagram.com/_kevintdo_/"
     summary = "Kevin is the scrum master for the P5 Slackbots. In his free time he likes do graphic design and play video games."
-    info = {"name": name, "firstname": firstname, "nickname": nickname, "github": github, "insta": insta, "summary": summary}
+    info = {"name": name, "color": color, "firstname": firstname, "nickname": nickname, "social1": social1, "social2": social2, "summary": summary}
     return info
 
 def abhijay():
     name = "abhijay deevi"
+    color = "#00a5f8"
     firstname = "abhijay"
     nickname = "sir animate"
-    github = "https://github.com/Dubshott"
-    insta = "https://www.instagram.com/dv_a_bee/"
+    social1 = "https://github.com/Dubshott"
+    social2 = "https://www.instagram.com/dv_a_bee/"
     summary = "Abhijay is the seckseeest and hawtest person in the world."
-    info = {"name": name, "firstname": firstname, "nickname": nickname, "github": github, "insta": insta, "summary": summary}
+    info = {"name": name, "color": color, "firstname": firstname, "nickname": nickname, "social1": social1, "social2": social2, "summary": summary}
     return info
 
 def kian():
     name = "kian kishimoto"
+    color = "#1bc725"
     firstname = "kian"
     nickname = "sir python"
-    github = "https://github.com/Uhpachee"
-    insta = "https://www.instagram.com/kian.isntcool/"
+    social1 = "https://github.com/Uhpachee"
+    social2 = "https://www.instagram.com/kian.isntcool/"
     summary = "Kian is the Kian."
-    info = {"name": name, "firstname": firstname, "nickname": nickname, "github": github, "insta": insta, "summary": summary}
+    info = {"name": name, "color": color, "firstname": firstname, "nickname": nickname, "social1": social1, "social2": social2, "summary": summary}
     return info
 
 def groupdata():
